bdfc30ec184ed846e05e8fed688b681e52401240
Novjean J Kannathara <novjeanj@gmail.com>
2024-07-05 13:37:02 +0530

    return value from coroutine

--- app/src/main/java/com/novatech/concurrencyamo/ExampleViewModel.kt
+++ app/src/main/java/com/novatech/concurrencyamo/ExampleViewModel.kt
@@ -1,10 +1,37 @@
 package com.novatech.concurrencyamo
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class ExampleViewModel: ViewModel() {
+    val TAG: String = "ExampleViewModel"
 
     init {
+//        viewModelScope.launch {
+//            val result = withContext(Dispatchers.IO) {
+//                delay(3000)
+//                true
+//            }
+//            // will receive a result of true in three seconds
+//            Log.d(TAG, "$result")
+//        }
 
+        val result = viewModelScope.async {
+            delay(3000)
+            true
+        }
+        result.invokeOnCompletion {
+            if(it==null){
+                Log.d(TAG, "${result.getCompleted()}")
+            }
+        }
     }
 }
